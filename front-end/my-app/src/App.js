08d76e074a9498349e613cc714a1c7808d8b1d72
ptonpe <tpranav45@gmail.com>
2024-10-30 10:53:53 -0500

    landing page rough draft

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <div className="navbar-brand">Mentorship App</div>
+        <ul className="navbar-nav">
+          <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+        </ul>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="header-title">Welcome to the Mentorship App</h1>
+        <p className="header-subtitle">Your journey to success starts here!</p>
+        <button className="cta-button">Get Started</button>
       </header>
     </div>
   );
