a27ec319c0d71d87f5b08a21beb7aa65bd2fc57b
Klarissa Tey <klarissatey@wireless-10-145-44-215.public.utexas.edu>
2024-12-03 17:14:19 -0600

    footer start

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -43,6 +43,22 @@ function MainContent() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <div class="footer-container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-body text-decoration-none lh-1">
+              <svg class="bi" width="30" height="24"><use xlinkHref="#bootstrap"/></svg>
+            </a>
+            <span class="mb-3 mb-md-0 text-body">&copy; 2024 Company, Inc</span>
+          </div>
+
+          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-body" href="#"><svg class="bi" width="24" height="24"><use xlinkHref="#twitter"/></svg></a></li>
+            <li class="ms-3"><a class="text-body" href="#"><svg class="bi" width="24" height="24"><use xlinkHref="#instagram"/></svg></a></li>
+            <li class="ms-3"><a class="text-body" href="#"><svg class="bi" width="24" height="24"><use xlinkHref="#facebook"/></svg></a></li>
+          </ul>
+        </footer>
+      </div>
     </div>
   );
 }
